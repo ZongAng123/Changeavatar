f19779a19f3711a9f53a3372fec03f3fe6acc9f6
ios <ios@iosdeMacBook-Pro.local>
2019-08-14 08:56:36 +0800

    头像更换

--- Change avatar/AppDelegate.h
+++ Change avatar/AppDelegate.h
@@ -12,6 +12,14 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+/**
+ echo "# Changeavatar" >> README.md
+ git init
+ git add README.md
+ git commit -m "first commit"
+ git remote add origin https://github.com/ZongAng123/Changeavatar.git
+ git push -u origin master
+ **/
 
 @end
 
